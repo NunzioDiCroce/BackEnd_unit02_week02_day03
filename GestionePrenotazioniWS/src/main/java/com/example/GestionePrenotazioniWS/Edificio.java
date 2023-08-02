8d947d4ce741ea128eb48f43f96cf31432a2c7ba
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-02 15:02:51 +0200

    Create Edificio.java

---
+++ GestionePrenotazioniWS/src/main/java/com/example/GestionePrenotazioniWS/Edificio.java
@@ -0,0 +1,49 @@
+package com.example.GestionePrenotazioniWS;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Table(name = "edifici")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+
+@Builder
+
+public class Edificio {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	protected String nome;
+	protected String indirizzo;
+	protected String citta;
+
+	@OneToMany
+	protected Set<Postazione> postazioni;
+
+	public Edificio(String _nome, String _indirizzo, String _citta) {
+		this.nome = _nome;
+		this.indirizzo = _indirizzo;
+		this.citta = _citta;
+	}
+
+}
