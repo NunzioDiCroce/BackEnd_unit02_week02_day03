6ef904de6cae14dd8ed67ea33332ca6827527083
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-02 16:24:13 +0200

    Update UtenteService.java

---
+++ GestionePrenotazioniWS/src/main/java/com/example/GestionePrenotazioniWS/entities/UtentePayload.java
@@ -0,0 +1,15 @@
+package com.example.GestionePrenotazioniWS.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
+public class UtentePayload {
+
+	protected String userName;
+	protected String nome;
+	protected String mail;
+
+}
