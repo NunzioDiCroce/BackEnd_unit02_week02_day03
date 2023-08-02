42220962ce4ba8f721bb9f21f0bb9fbb0db9f76d
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-02 14:24:22 +0200

    Create project

---
+++ GestionePrenotazioniWS/src/main/java/com/example/GestionePrenotazioniWS/GestionePrenotazioniWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionePrenotazioniWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionePrenotazioniWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionePrenotazioniWsApplication.class, args);
+	}
+
+}
---
+++ GestionePrenotazioniWS/src/test/java/com/example/GestionePrenotazioniWS/GestionePrenotazioniWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionePrenotazioniWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotazioniWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
