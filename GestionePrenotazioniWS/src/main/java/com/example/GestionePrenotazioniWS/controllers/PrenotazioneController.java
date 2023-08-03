e6fdec29b151ae754c52d7d620bf5d1d66769ffd
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-03 08:21:39 +0200

    Create PrenotazioneController.java

---
+++ GestionePrenotazioniWS/src/main/java/com/example/GestionePrenotazioniWS/controllers/PrenotazioneController.java
@@ -0,0 +1,10 @@
+package com.example.GestionePrenotazioniWS.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/prenotazioni")
+public class PrenotazioneController {
+
+}
